2479f02536334cb396ee53cdf7eed892f15dc461
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2022-08-31 19:59:48 -0700

    Class attr_accessor

--- RubyUdemy/Classes/gadget.rb
+++ RubyUdemy/Classes/gadget.rb
@@ -1,14 +1,34 @@
 class Gadget
-  def initialize
-    @username = "User #{rand(1..100)}"
-    @password = "topsecret"
+
+  attr_accessor :username
+  attr_reader :production_number
+  attr_writer :password
+
+  def initialize(username, password)
+    @username = username
+    @password = password
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
   def to_s
-    "Gadget #{@production_number} has the username #{@username}."
+    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}."
   end
 end
 
-phone = Gadget.new
-puts phone.to_s
+g1 = Gadget.new("RubyTime", "Prog")
+p g1.username
+p g1.production_number
+g1.password = "blahBlah"
+g1.username = "AmericanHustle"
+p g1.username
+
+g2 = Gadget.new("mrprogram", "bestpass")
+g2.username = "Cray"
+p g2.username
+
+g3 = Gadget.new("sports", "topsecret")
+p g3.production_number
+
+p g1.username
+p g2.username
+p g3.username
