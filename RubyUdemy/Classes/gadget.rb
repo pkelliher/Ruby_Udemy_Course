a17a28369e3c3b836bfc2fee65b46834fa37cacb
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2022-09-04 19:04:29 -0700

    part of classes part 2

--- RubyUdemy/Classes/gadget.rb
+++ RubyUdemy/Classes/gadget.rb
@@ -1,34 +1,48 @@
 class Gadget
 
-  attr_accessor :username
+  #attr_writer :password
   attr_reader :production_number
-  attr_writer :password
+  attr_accessor :username, :password
+
 
   def initialize(username, password)
     @username = username
     @password = password
-    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+    @production_number = generate_production_number
   end
 
   def to_s
-    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}."
+    "Gadget #{@production_number} has the username #{@username}.
+    It is made from the #{self.class} class and it
+    has the ID #{self.object_id}."
   end
+
+  def password=(new_password)
+    @password = new_password if validate_password(new_password)
+  end
+
+  private
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."Z").to_a
+    middle_digits = "2017"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
+  end
+
 end
 
-g1 = Gadget.new("RubyTime", "Prog")
-p g1.username
-p g1.production_number
-g1.password = "blahBlah"
-g1.username = "AmericanHustle"
-p g1.username
+phone = Gadget.new("rubytime123", "patrick123")
+puts phone.password
 
-g2 = Gadget.new("mrprogram", "bestpass")
-g2.username = "Cray"
-p g2.username
+phone.password = 123
+puts phone.password
 
-g3 = Gadget.new("sports", "topsecret")
-p g3.production_number
-
-p g1.username
-p g2.username
-p g3.username
+phone.password = "test123"
+puts phone.password
